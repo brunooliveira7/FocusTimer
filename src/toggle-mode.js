28bdf7b92ca98e5873ba56b50b2484210e44bceb
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-09-02 10:22:12 -0300

    Adding light mode logic

---
+++ src/toggle-mode.js
@@ -0,0 +1,13 @@
+let darkMode = true; // app começa no darkMode
+const buttonToggle = document.querySelector("#toggle-mode");
+
+buttonToggle.addEventListener("click", (event) => {
+  document.documentElement.classList.toggle("light");
+
+  // só para mudar o texto da acessibilidade
+  const mode = darkMode ? "light" : "dark";
+  event.currentTarget.querySelector(
+    "span"
+  ).textContent = `${mode} mode ativado`;
+  darkMode = !darkMode;
+});
