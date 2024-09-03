562558a58808d013f096997c93f85a73fe8660de
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-09-03 19:52:30 -0300

    Clearing the content of a tag with a focus event

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,12 +1,20 @@
 import { controls } from "./elements.js";
 import * as actions from "./actions.js";
+import * as el from "./elements.js";
 
 export function registerControls() {
   controls.addEventListener("click", (event) => {
     const action = event.target.dataset.action; //verifica se clicou nos botões
-    if (typeof actions[action] != "function") { //verifica se a ação/funct existe
+    //verifica se a ação/funct existe
+    if (typeof actions[action] != "function") {
       return;
     }
     actions[action]();
+  });
+}
+
+export function setMinutes() {
+  el.minutes.addEventListener("focus", () => {
+    el.minutes.textContent = "";
   });
 }
