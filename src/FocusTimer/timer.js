5fa99c3372dc4ebaa694e700b67baf7d61425c50
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-09-03 11:13:38 -0300

    Structuring the countdown

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,6 +1,15 @@
 import state from "./state.js";
 import * as el from "./elements.js";
 
+export function countdown() {
+  if (!state.isRunning) {
+    return;
+  }
+  console.log("iniciou");
+  // funct que executa uma funct depois de um determinado tempo (1000=1s) - recursão/callback - chama ela mesma
+  setTimeout(() => countdown(), 1000);
+}
+
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes; // ?? -nullish coalesing operator - verifica se o parâm. é null - se sim coloca parâm. válido no lugar do null
   seconds = seconds ?? state.seconds;
